e80a4949caed02d1c7095178ffcaf624288069ae
user_name <email_id>
2015-10-14 15:45:04 +0300

    first commit

---
+++ register.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>User Login</title>
+<link rel="stylesheet" type="text/css" href="../CEASER BIRORI/styles.css" />
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<div class="message"></div>
+<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
+<tr class="tableheader">
+<td colspan="2" align="center" bgcolor="#0033CC"><strong><b><font size="35">Enter Your Details</font></b></strong></td>
+</tr>
+<tr class="tablerow">
+<td align="right" bgcolor="#FFFF00">Username</td>
+<td bgcolor="#FFFF00"><input type="text" name="user_name"></td>
+</tr>
+<tr class="tablerow">
+<td align="right" bgcolor="#FFFF00">Password</td>
+<td bgcolor="#FFFF00"><input type="password" name="password"></td>
+</tr>
+<tr class="tableheader">
+<td colspan="2" align="center" bgcolor="#FFFF00"><input type="submit" name="submit" value="Send"></td>
+</tr>
+<tr class="tableheader">
+<td colspan="2" align="center" bgcolor="#CC0066"><a href="../CEASER BIRORI/register.php">Sign Up</a></td>
+</tr>
+</table>
+</form>
+</body></html>
